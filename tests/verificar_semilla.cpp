508489e900b99f8032a58a29d9a84021c8d3cb26
Cristobal Machado Sanchez <cmachados@eafit.edu.co>
2026-08-30 15:08:31 -0500

    fix: eliminar espacio extra en cadena de apellidos e ignorar binario

--- tests/verificar_semilla.cpp
+++ tests/verificar_semilla.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    string apellidos = "gomezgonzalesmachadorodriguezsanchezsanchez ";
+    string apellidos = "gomezgonzalesmachadorodriguezsanchezsanchez";
 
     long long suma_ascii = 0;
 
